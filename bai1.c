904c4742e078054f6889de7a7c4d4191ebfdbf57
isaysgg5times <tu50983@gmail.com>
2025-03-03 22:06:54 +0700

    Add files via upload

---
+++ bai1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main()
+{
+    int x;
+    printf("Nhap gia tri");
+    scanf("%d",&x);
+    printf("%d %d %d",x*x,x*x*x,x*x*x*x);
+    return 0;
+}
+
+
+
+
